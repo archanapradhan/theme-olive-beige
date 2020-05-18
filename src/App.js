f3fd0139d79afb36bc59516812fefa8ec598ff52
Archana Pradhan <mail.archanapradhan@gmail.com>
2020-05-18 15:30:21 +1200

    customized theme over bootswatch and bootstrap

--- src/App.js
+++ src/App.js
@@ -2836,39 +2836,10 @@ function App() {
               <li className="float-lg-right">
                 <a href="#top">Back to top</a>
               </li>
-              <li>
-                <a
-                  href="https://blog.bootswatch.com"
-                  onclick="pageTracker._link(this.href); return false;"
-                >
-                  Blog
-                </a>
-              </li>
-              <li>
-                <a href="https://blog.bootswatch.com/rss/">RSS</a>
-              </li>
-              <li>
-                <a href="https://twitter.com/bootswatch">Twitter</a>
-              </li>
-              <li>
-                <a href="https://github.com/thomaspark/bootswatch/">GitHub</a>
-              </li>
-              <li>
-                <a href="../help/#api">API</a>
-              </li>
-              <li>
-                <a href="../help/#donate">Donate</a>
-              </li>
             </ul>
             <p>
-              Made by <a href="https://thomaspark.co">Thomas Park</a>.
-            </p>
-            <p>
-              Code released under the{" "}
-              <a href="https://github.com/thomaspark/bootswatch/blob/master/LICENSE">
-                MIT License
-              </a>
-              .
+              Customized by Archana Pradhan, inspired by bootswatch and
+              bootstrap.
             </p>
             <p>
               Based on{" "}
